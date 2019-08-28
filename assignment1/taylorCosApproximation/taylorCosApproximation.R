e040ccbdbd4c48c2e4b15047f69606a4060dd819
James Shirk <jshirk1@student.gsu.edu>
2019-08-28 16:26:30 -0400

    file restructure

--- assignment1/taylorCosApproximation/taylorCosApproximation.R
+++ assignment1/taylorCosApproximation/taylorCosApproximation.R
@@ -13,5 +13,9 @@ taylorSeries1 <- data.frame(t)
 
 comparisonPlot <- ggplot() +
     geom_line(data=cosine, aes(x=t, y=y, group=1), colour="#FF0000") +
-    geom_line(data=taylorSeries, aes(x=t, y=y, group=1), colour="#FF0000") +
+    #geom_line(data=taylorSeries, aes(x=t, y=y, group=1), colour="#FF0000") +
+    labs(x = "x", y = "cos(x)", title = "plot of cos(x)") 
 
+ggsave("cosine.png", plot = comparisonPlot, device = NULL, path = NULL,
+  scale = 1, width = 20, height = 10, units = c("cm"),
+  dpi = 300, limitsize = TRUE)
