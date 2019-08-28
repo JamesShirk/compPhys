8851fdf16538a67ffd15049605712e0087dccdab
James Shirk <jamesshirk72@gmail.com>
2019-08-27 20:39:43 -0400

    finished problem 3, calculation of the standard deviation, todo: try for extra credit through third implementation

---
+++ assignment1/stDev/randomList.py
@@ -0,0 +1,16 @@
+# James Shirk, August 27, 2019 for Dr. Pratt Computational Physics class assignment 1
+
+# Random Module used to generate random numbers
+# The default random seed is based on the time since none is provided
+import random
+
+def randomList():
+    # Opens a file called samples.txt and then iterates one million times writing a random 
+    # number between 0 and 1000 on a seperate line
+    with open("samples.txt","w+") as f:
+        for i in range(0,1000000):
+            f.write(str(random.randint(0,1001))+"\n")
+
+# Executes the function
+if __name__ == "__main__":
+    randomList()
--- assignment1/stDev/stDev1.py
+++ assignment1/stDev/stDev1.py
@@ -1,14 +1,52 @@
+# James Shirk, August 27, 2019 for Dr. Pratt Computational Physics class assignment 1
 
+# Imports the square root function from the math module
+from math import sqrt
+import statistics
 
-def stDev(*args):
+# Open the 'samples.txt' file and append each entry to a list.
+with open("samples.txt","r") as f:
+    samples = []
+    for line in f:
+        # Take each line, remove the newline character, convert it to an int and append it to a list
+        samples.append(int(line.strip()))
+
+# The first method of standard deviation implementation
+def stDev1(samps):
     sum = 0
     sumsq = 0
-    for x in args:
-        sum += x
-        sumsq += x**2
-    N = len(args)
+    N = len(samps)
+    # iterate over each element in the list and sum each element and each element squared
+    for i in samps:
+        sum += i
+        sumsq += i**2
+    # Calculate the mean and standard deviation
     mean = sum / N
-    return ((sumsq - N*mean**2)/(N-1))
+    stDev = sqrt((sumsq - N * (mean**2))/(N-1))
+    # Rather than returning the mean and the stdev, I print them to the console
+    # This simplifies the code by removing the need to return two variables
+    print("From the first method: The mean is " + str(mean) + ", the standard deviation is " + str(stDev))
 
+# The second implementation of standard deviation implementation
+def stDev2(samps):
+    mean = 0
+    stDev = 0
+    N = len(samps)
+    # iterate over each element, the second argument of range is exclusive so you must add one
+    # Find the mean by calculating the mean and then changing the mean up or down due to the impact from each element 
+    for i in range(1, (N + 1)):
+        j = samps[(i-1)]
+        oldMean = mean
+        mean += (j-mean)/i
+        stDev += (j-mean)*(j-oldMean)
+    # finalize calculation of stDev by dividing it by the elements and taking the square root
+    stDev /= (N - 1)
+    stDev = sqrt(stDev)
+    # Rather than returning the mean and the stdev, I print them to the console
+    # This simplifies the code by removing the need to return two variables
+    print("From the second method: The mean is " + str(mean) + ", the standard deviation is " + str(stDev))
+
+# Execute the functions
 if __name__ == "__main__":
-    stDev(*args):
+    stDev1(samples)
+    stDev2(samples)  
