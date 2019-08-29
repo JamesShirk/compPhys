8ff31a9b79e2012fdb893168c95cbeed136336f6
James Shirk <jshirk1@student.gsu.edu>
2019-08-29 14:49:16 -0400

    Wrote the R code to create graphs and calculate error for the taylor approximation, todo: comment code

--- assignment1/taylorCosApproximation/taylorCosApproximation.R
+++ assignment1/taylorCosApproximation/taylorCosApproximation.R
@@ -1,21 +1,76 @@
-# James Shirk, August 27, 2019 for Dr. Pratt Computational Physics class assignment 1, problem 2
+# James Shirk, August 27-29, 2019 for Dr. Pratt Computational Physics class assignment 1, problem 2
 
 library(ggplot2)
 library(scales)
 library(pracma)
 
-t = seq(0,10,.1)
-y = cos(t)
+taylorOrder0 <- read.table(file="taylorOutputOrder0.txt", head=FALSE, sep=",")
+taylorOrder3 <- read.table(file="taylorOutputOrder3.txt", head=FALSE, sep=",")
+taylorOrder9 <- read.table(file="taylorOutputOrder9.txt", head=FALSE, sep=",")
+numbers <- read.table(file="numberList.txt", head=FALSE)
 
-cosine <- data.frame(t,y)
-taylorSeries1 <- data.frame(t)
+y <- cos(numbers)
 
+cosine <- data.frame(numbers,y)
+taylorSeries0 <- data.frame(numbers,taylorOrder0)
+taylorSeries3 <- data.frame(numbers,taylorOrder3)
+taylorSeries9 <- data.frame(numbers,taylorOrder9)
 
-comparisonPlot <- ggplot() +
-    geom_line(data=cosine, aes(x=t, y=y, group=1), colour="#FF0000") +
-    #geom_line(data=taylorSeries, aes(x=t, y=y, group=1), colour="#FF0000") +
-    labs(x = "x", y = "cos(x)", title = "plot of cos(x)") 
+error0 = merge(cosine, taylorSeries0, by = "V1")
+colnames(error0) <- c("numbers","taylor0","cosine")
+error0[ ,4] <- error0$cosine - error0$taylor0
+error0[ ,4] <- error0[ ,4] / error0$cosine
+error0[ ,4] <- error0[ ,4] * 100
+cat("The mean error of the leading order is: ",mean(error0[ ,4]), "%\n")
 
-ggsave("cosine.png", plot = comparisonPlot, device = NULL, path = NULL,
+error3 = merge(cosine, taylorSeries3, by = "V1")
+colnames(error3) <- c("numbers","taylor3","cosine")
+error3[ ,4] <- error3$cosine - error3$taylor3
+error3[ ,4] <- error3[ ,4] / error3$cosine
+error3[ ,4] <- error3[ ,4] * 100
+cat("The mean error of the fourth order is: ",mean(error3[ ,4]), "%\n")
+
+error9 = merge(cosine, taylorSeries9, by = "V1")
+colnames(error9) <- c("numbers","taylor9","cosine")
+error9[ ,4] <- error9$cosine - error9$taylor9
+error9[ ,4] <- error9[ ,4] / error9$cosine
+error9[ ,4] <- error9[ ,4] * 100
+cat("The mean error of the tentth order is: ",mean(error9[ ,4]), "%\n")
+
+cosineTaylor0 <- ggplot() +
+    geom_line(data=cosine, aes(x=V1, y=V1.1, group=1, colour="#FF0000")) +
+    geom_line(data=taylorSeries0, aes(x=V1, y=V1.1, group=1, colour="#0000FF")) +
+    scale_color_discrete(name = "Comparison", labels = c("cos(x)", "Leading Order Taylor Approx")) +
+    geom_hline(yintercept = 0) +
+    theme_bw() +
+    labs(x = "x", y = "y", title = "Leading Order Taylor Cosine Approximation") 
+
+cosineTaylor3 <- ggplot() +
+    geom_line(data=cosine, aes(x=V1, y=V1.1, group=1, colour="#FF0000")) +
+    geom_line(data=taylorSeries3, aes(x=V1, y=V1.1, group=1, colour="#0000FF")) +
+    scale_color_discrete(name = "Comparison", labels = c("cos(x)", "Fourth Order Taylor Approx")) +
+    geom_hline(yintercept = 0) +
+    theme_bw() +
+    labs(x = "x", y = "y", title = "Fourth Order Taylor Cosine Approximation") 
+
+cosineTaylor9 <- ggplot() +
+    geom_line(data=cosine, aes(x=V1, y=V1.1, group=1, colour="#FF0000")) +
+    geom_line(data=taylorSeries9, aes(x=V1, y=V1.1, group=1, colour="#0000FF")) +
+    scale_color_discrete(name = "Comparison", labels = c("cos(x)", "Tenth Order Taylor Approx")) +
+    geom_hline(yintercept = 0) +
+    theme_bw() +
+    labs(x = "x", y = "y", title = "Tenth Order Taylor Cosine Approximation") 
+
+ggsave("cosineTaylor0.png", plot = cosineTaylor0, device = NULL, path = NULL,
   scale = 1, width = 20, height = 10, units = c("cm"),
   dpi = 300, limitsize = TRUE)
+
+ggsave("cosineTaylor3.png", plot = cosineTaylor3, device = NULL, path = NULL,
+  scale = 1, width = 20, height = 10, units = c("cm"),
+  dpi = 300, limitsize = TRUE)
+
+ggsave("cosineTaylor9.png", plot = cosineTaylor9, device = NULL, path = NULL,
+  scale = 1, width = 20, height = 10, units = c("cm"),
+  dpi = 300, limitsize = TRUE)
+
+
--- assignment1/taylorCosApproximation/taylorCosData.py
+++ assignment1/taylorCosApproximation/taylorCosData.py
@@ -1,10 +1,13 @@
+# James Shirk, August 28, 2019 for Dr. Pratt Computational Physics class assignment 1, problem 2
+# Generates Data for the problem
+
 import math
 import numpy as np
 
 # Main Function, takes in order and outputs results from -5 to 5 incrementing by .1 for the taylor cos approximation
 def TaylorSeries(order):
     # Make an array of numbers from 0 to 10 incrementing by .1
-    numbers = np.linspace(-5,5,101)
+    numbers = np.linspace(-5,5,1001)
     taylorOutput = []
     # The outer for loop iterates over each element in the 'numbers' list
     # The innter for loop takes in the order and does the mathematical summation
