1a262e28fc1295dbd76fac0623a9053dd46dafdd
James Shirk <jshirk1@student.gsu.edu>
2019-08-30 12:41:07 -0400

    updated R code to make arbitrary graph with integer input for order wherer leading order is 0. todo: implement integration and comment code

---
+++ assignment1/taylorCosApproximation/final/TaylorCosApproximation.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(scales)
+library(pracma)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+taylor = function(x, order) {
+  coefs = (-1)^(0:order) / factorial(2 * (0:order))
+  x_grid = outer(x, 2 * (0:order), "^")
+  x_grid %*% coefs
+}
+
+cosine <- function(x) {
+    base::cos(x)
+}
+
+plot0 <- ggplot(data = data.frame(x = 0), mapping = aes(x = x)) +
+    stat_function(fun = taylor, geom = "line", args = list(order = strtoi(args[1])), mapping = aes(color = "taylor")) +
+    stat_function(fun = cosine, geom = "line", linetype = "dashed", mapping = aes(color = "cosine")) +
+    scale_x_continuous(limits = c(-pi/2, pi/2)) +
+    scale_color_manual(name = "Functions", values = c("taylor" = "#FF0000", "cosine" = "#0000FF"), labels = c("cos(x)", paste("order", strtoi(args[1]) + 1,"\ntaylor approx"))) +
+    theme_bw() +
+    labs(x = "x", y = "y", title = paste("Order", strtoi(args[1]) + 1, "Taylor Cosine Approximation")) 
+
+ggsave(paste0("taylorCosine",args[1],".png"), plot = plot0, device = NULL, path = NULL,
+    scale = 1, width = 20, height = 10, units = c("cm"),
+    dpi = 300, limitsize = TRUE)
+
+#print(integrate(f0cos, lower = -pi/2, upper = pi/2))
--- assignment1/taylorCosApproximation/firstTry/taylorCosApproximationOLD.R
+++ assignment1/taylorCosApproximation/firstTry/taylorCosApproximationOLD.R
@@ -7,6 +7,7 @@ library(pracma)
 taylorOrder0 <- read.table(file="taylorOutputOrder0.txt", head=FALSE, sep=",")
 taylorOrder3 <- read.table(file="taylorOutputOrder3.txt", head=FALSE, sep=",")
 taylorOrder9 <- read.table(file="taylorOutputOrder9.txt", head=FALSE, sep=",")
+taylorOrder50 <- read.table(file="taylorOutputOrder50.txt", head=FALSE, sep=",")
 numbers <- read.table(file="numberList.txt", head=FALSE)
 
 y <- cos(numbers)
@@ -15,6 +16,7 @@ cosine <- data.frame(numbers,y)
 taylorSeries0 <- data.frame(numbers,taylorOrder0)
 taylorSeries3 <- data.frame(numbers,taylorOrder3)
 taylorSeries9 <- data.frame(numbers,taylorOrder9)
+taylorSeries50 <- data.frame(numbers,taylorOrder50)
 
 error0 = merge(cosine, taylorSeries0, by = "V1")
 colnames(error0) <- c("numbers","taylor0","cosine")
@@ -36,6 +38,13 @@ error9[ ,4] <- error9$cosine - error9$taylor9
 error9[ ,4] <- error9[ ,4] / error9$cosine
 error9[ ,4] <- error9[ ,4] * 100
 cat("The mean error of the tentth order is: ",mean(error9[ ,4]), "%\n")
+
+error50 = merge(cosine, taylorSeries9, by = "V1")
+colnames(error50) <- c("numbers","taylor9","cosine")
+error50[ ,4] <- error50$cosine - error50$taylor9
+error50[ ,4] <- error50[ ,4] / error50$cosine
+error50[ ,4] <- error50[ ,4] * 100
+cat("The mean error of the fifty-first order is: ",mean(error50[ ,4]), "%\n")
 
 cosineTaylor0 <- ggplot() +
     geom_line(data=cosine, aes(x=V1, y=V1.1, group=1, colour="#FF0000")) +
@@ -61,6 +70,14 @@ cosineTaylor9 <- ggplot() +
     theme_bw() +
     labs(x = "x", y = "y", title = "Tenth Order Taylor Cosine Approximation") 
 
+cosineTaylor50 <- ggplot() +
+    geom_line(data=cosine, aes(x=V1, y=V1.1, group=1, colour="#FF0000")) +
+    geom_line(data=taylorSeries50, aes(x=V1, y=V1.1, group=1, colour="#0000FF")) +
+    scale_color_discrete(name = "Comparison", labels = c("cos(x)", "Fifty-first Order Taylor Approx")) +
+    geom_hline(yintercept = 0) +
+    theme_bw() +
+    labs(x = "x", y = "y", title = "Fifty-first Order Taylor Cosine Approximation") 
+
 ggsave("cosineTaylor0.png", plot = cosineTaylor0, device = NULL, path = NULL,
   scale = 1, width = 20, height = 10, units = c("cm"),
   dpi = 300, limitsize = TRUE)
@@ -73,4 +90,7 @@ ggsave("cosineTaylor9.png", plot = cosineTaylor9, device = NULL, path = NULL,
   scale = 1, width = 20, height = 10, units = c("cm"),
   dpi = 300, limitsize = TRUE)
 
+ggsave("cosineTaylor50.png", plot = cosineTaylor50, device = NULL, path = NULL,
+  scale = 1, width = 20, height = 10, units = c("cm"),
+  dpi = 300, limitsize = TRUE)
 
