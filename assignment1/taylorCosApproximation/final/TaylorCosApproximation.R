4d1ab7283b020e7d838dca56e029473eeb0883a3
James Shirk <jamesshirk72@gmail.com>
2019-09-01 23:12:48 -0400

    integration test

--- assignment1/taylorCosApproximation/final/TaylorCosApproximation.R
+++ assignment1/taylorCosApproximation/final/TaylorCosApproximation.R
@@ -7,14 +7,12 @@ library(pracma)
 args <- commandArgs(trailingOnly = TRUE)
 
 # For a given input 'order' returns the the order of the taylor series for that given order
-# Coefs creates a 1 x Order matrix with the coefficients of that order of the taylor series
-# x_grid creates a 
+# Coefs calculates the coefficients for each element of the series
+# 
 
 taylor = function(x, order) {
   coefs = (-1)^(0:order) / factorial(2 * (0:order))
-  print(coefs)
   x_grid = outer(x, 2 * (0:order), "^")
-  print(x_grid)
   x_grid %*% coefs
 }
 
@@ -34,4 +32,4 @@ ggsave(paste0("taylorCosine",args[1],".png"), plot = plot0, device = NULL, path 
     scale = 1, width = 20, height = 10, units = c("cm"),
     dpi = 300, limitsize = TRUE)
 
-#print(integrate((taylor - cosine), lower = -pi/2, upper = pi/2))
+print(integrate((taylor - cosine), lower = -pi/2, upper = pi/2))
