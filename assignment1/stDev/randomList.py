7432ffbe24998ec63b895b69bc1e441d85630664
James Shirk <jamesshirk72@gmail.com>
2019-09-02 23:53:12 -0400

    added code to make a plot of the percent difference as a function of order

--- assignment1/stDev/randomList.py
+++ assignment1/stDev/randomList.py
@@ -1,4 +1,5 @@
 # James Shirk, August 27, 2019 for Dr. Pratt Computational Physics class assignment 1, problem 3
+# Generates 1 million random numbers
 
 # Random Module used to generate random numbers
 # The default random seed is based on the time since none is provided
@@ -8,7 +9,7 @@ def randomList():
     # Opens a file called samples.txt and then iterates one million times writing a random 
     # number between 0 and 1000 on a seperate line
     with open("samples.txt","w+") as f:
-        for i in range(0,1000000):
+        for x in range(0,1000000):
             f.write(str(random.randint(0,1001))+"\n")
 
 # Executes the function
