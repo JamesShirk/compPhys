9d222d07e3c012d3a9487b94ef7e17e225148425
James Shirk <jamesshirk72@gmail.com>
2019-10-01 01:28:58 -0400

    fuck this

--- assignment3/fourierTransform/FFT.py
+++ assignment3/fourierTransform/FFT.py
@@ -1,9 +1,13 @@
-from numpy.fft import fft, fftfreq
-from numpy import arange, delete
-from math import sin, sqrt
-import matplotlib.pyplot as plt
-import pylab
+# James Shirk for computational physics class September 28-30
+# computes FT 
 
+from numpy.fft import fft, fftfreq   # For computing Fourier transform
+from numpy import arange             # for making lists of equally spaced numbers
+from math import sin, sqrt           # for sin and sqrt
+import matplotlib.pyplot as plt      # for plotting
+from time import time                # Time differences
+
+# Haar function
 def function(x):
     if(x >= 0 and x < .5):
         return 1
@@ -12,32 +16,25 @@ def function(x):
     else:
         return 0
 
-def main():
+# Main fourier transform
+def fourierTransform():
+    # When computing the time shifting, I changed values = arange(-5, 5, .1) to -6, 4, .1
     values = arange(-5, 5, .1)
+    # Computes the value of the function for each element in values
     x = [function(i) for i in values]
+    # Computes the frequency using the original values as the time
     freq = fftfreq(values.shape[-1])
+    # Function that computes the fourier transform
     output = fft(x)
+    # Takes the magnitude
     magnitude = [sqrt(output.real[i]**2 + output.imag[i]**2) for i in range(0, len(output))]
+    # Plots
     plt.plot(freq, magnitude, 'o')
     plt.xlim(-.6, .6)
     plt.xlabel('Frequency')
     plt.ylabel('Magnitude of Fourier Transform Function')
     plt.title('Fourier Transform')
     plt.savefig('magnitudeFFT.png')
-    plt.close()
-
-def timeShifting():
-    values = arange(-6, 4, .1)
-    x = [function(i) for i in values]
-    freq = fftfreq(values.shape[-1])
-    output = fft(x)
-    magnitude = [sqrt(output.real[i]**2 + output.imag[i]**2) for i in range(0, len(output))]
-    plt.plot(freq, magnitude, 'o')
-    plt.xlim(-.6, .6)
-    plt.xlabel('Frequency')
-    plt.ylabel('Magnitude of Fourier Transform Function')
-    plt.title('Time Scaled Fourier Transform')
-    plt.savefig('timeShiftedFT.png')
     plt.close()
 
 def functionPlot():
@@ -52,8 +49,11 @@ def functionPlot():
     plt.close()
 
 if __name__ == "__main__":
-    main()
-    timeShifting()
-    functionPlot()
-
+    timeTotal = 0
+    for x in range(0, 1000):
+        t1 = time()
+        fourierTransform()
+        t2 = time()
+        timeTotal += (t2 - t1)
+    print("The program took " + str(timeTotal) + " milliseconds.")
 
