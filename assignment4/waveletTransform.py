e8c50089e5bfe1504e07d4d42522698b051bc109
James Shirk <jshirk1@student.gsu.edu>
2019-10-08 17:16:57 -0400

    began wavelet transfrom

---
+++ assignment4/waveletTransform.py
@@ -0,0 +1,32 @@
+from math import sqrt, exp, pi
+import matplotlib.pyplot as plt
+from numpy import arange
+import pywt
+import scipy
+
+def function(x, a):
+    return ((2/((pi ** .25)*sqrt(3*a)))*(1 - (x**2/a**2))*exp((-x**2)/(2*(a**2))))
+
+def main():
+    x = arange(-5, 5, .1)
+    yFamily = [function(i, 1) for i in x]
+    y = [function(i, .5) for i in x]
+
+    coef, freqs = pywt.cwt(y, arange(1,128),'mexh')
+
+    plt.matshow(coef)
+    plt.savefig("test.png", dpi = 300)
+    plt.close()
+
+    plt.plot(x, y, "--")
+    plt.axhline(y=0, color='black', linestyle='-')
+    plt.savefig("function.png", dpi = 300)
+    plt.close()
+
+    plt.plot(x, yFamily, "--")
+    plt.axhline(y=0, color='black', linestyle='-')
+    plt.savefig("mexicanHat.png", dpi = 300)
+    plt.close()
+
+if __name__ == "__main__":
+    main()
