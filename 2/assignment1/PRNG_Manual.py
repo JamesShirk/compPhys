72a53cf18dfafaeb0d5275eb7294d5fe981c8358
James Shirk <jamesshirk72@gmail.com>
2020-01-22 11:50:55 -0500

    small changes

--- 2/assignment1/PRNG_Manual.py
+++ 2/assignment1/PRNG_Manual.py
@@ -27,7 +27,7 @@ def psuedoRanXOR(seed, m1, p, q, n, upperLimit, m2 = None):
     m2 = m1 if not isinstance(m2, int) else m2
     # Generates the intial random data using the simple Fibbonacci generator
     nInitial = p + 1 if p > q else q + 1
-    initial = psuedoRanSum(seed, m1, nInitial, p)
+    initial = psuedoRanSum(seed, m1, nInitial, p, q)
     # Appends values to the end of the list equal to (Xp XOR Xq mod m2)
     for _ in range(0, n):
         initial.append((initial[len(initial) - p] ^ initial[len(initial) - q]) % m2)
@@ -37,9 +37,9 @@ def psuedoRanXOR(seed, m1, p, q, n, upperLimit, m2 = None):
     return(output[nInitial:n + nInitial])
 
 # Generates Fibbonacci values using a seed as x0 and seed mod p as x1 then just x2 = x1 + x0 mod m
-def psuedoRanSum(seed, m, n, p):
+def psuedoRanSum(seed, m, n, p, q):
     x0 = seed
-    x1 = seed % p
+    x1 =  seed % p if seed % 2 == 0 else seed % q
     x2 = 0
     values = []
     for _ in range(0, n):
